5b75cd2442c1ae1c00097ee247deaefc703ec614
nv22200 <154304439+nv22200@users.noreply.github.com>
2024-05-04 17:40:05 +0000

    34 - show all posts in home page

--- routes/PostRoutes.js
+++ routes/PostRoutes.js
@@ -5,10 +5,11 @@ import {
   updatePost,
   deletePost,
   getPost,
+  getAllPosts,
 } from "../controllers/PostControllers";
 import { authGuard, adminGuard } from "../middleware/authMiddleware";
 
-router.post("/", authGuard, adminGuard, createPost);
+router.route("/").post(authGuard, adminGuard, createPost).get(getAllPosts);
 router
   .route("/:slug")
   .put(authGuard, adminGuard, updatePost)
